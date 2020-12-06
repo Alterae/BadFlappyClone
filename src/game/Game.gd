b515fb5ab61466451faa6d0f36c690e54e3a2e76
Michelle S <progamer.firestorm@gmail.com>
2020-12-06 11:44:46 -0700

    Pipes spawn less frequently, have random heighs.

--- src/game/Game.gd
+++ src/game/Game.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+var position_spawn := Vector2(512, 135)
+var gate_preload := preload("res://entities/pipes/pipegate/PipeGate.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+
+# Called when the spawn timer times out.  Spawns a pipe.
+func _on_SpawnTimer_timeout() -> void:
+	var gate_instance := gate_preload.instance()
+	gate_instance.position = position_spawn
+	gate_instance.position.x += randi() % 100 - 100  # SHOULD give a random offset.
+	add_child(gate_instance)
