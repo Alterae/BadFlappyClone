b84eb31d648d372ba519dee44e25d2cc3f60d1fe
Michelle S <progamer.firestorm@gmail.com>
2020-12-06 11:00:08 -0700

    Bird can now die

--- src/entities/bird/Bird.gd
+++ src/entities/bird/Bird.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 var velocity := Vector2.ZERO
 var strength_jump := 200.0
 var gravity := 9.8
+var dead := false
+
+signal died
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,9 +15,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = -strength_jump
-	else:
-		velocity.y += gravity
+	if not dead:
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = -strength_jump
+		else:
+			velocity.y += gravity
 
-	velocity = move_and_slide(velocity)
+		velocity = move_and_slide(velocity)
+
+
+# Kills the player when we hit something.
+func die():
+	dead = true
+	emit_signal("died")
