844a92703b79fcb6c1112e93c8e1b06d5276c3d4
Michelle S <progamer.firestorm@gmail.com>
2020-12-06 10:52:53 -0700

    Tweak player jump force to actually be playable

--- src/entities/bird/Bird.gd
+++ src/entities/bird/Bird.gd
@@ -1,21 +1,20 @@
 extends KinematicBody2D
 
-
 var velocity := Vector2.ZERO
-var strength_jump := 10.0
+var strength_jump := 200.0
 var gravity := 9.8
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	if (Input.is_action_just_pressed("ui_accept")):
+	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = -strength_jump
 	else:
 		velocity.y += gravity
-	
+
 	velocity = move_and_slide(velocity)
