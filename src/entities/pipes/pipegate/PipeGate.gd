9d60aca1adb00ccd09b6d586d97fcee9afcad08a
Michelle S <progamer.firestorm@gmail.com>
2020-12-06 11:27:01 -0700

    Changed references to simplify code

--- src/entities/pipes/pipegate/PipeGate.gd
+++ src/entities/pipes/pipegate/PipeGate.gd
@@ -2,14 +2,12 @@ extends Node2D
 
 var size_gap := 32.0  # TODO: Set up random or difficulty/based variation.
 var speed := 10
-onready var segment_top := $TopSegment
-onready var segment_bottom := $BottomSegment
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	segment_top.position.y -= size_gap / 2
-	segment_bottom.position.y += size_gap / 2
+	$TopSegment.y -= size_gap / 2
+	$BottomSegment.y += size_gap / 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
