26fa15643bc4d605b5a8a4cdaf6aaa8ac8e280c3
Michelle S <progamer.firestorm@gmail.com>
2020-12-06 11:09:26 -0700

    Set up PipeSegment

---
+++ src/entities/pipes/pipesegment/PipeSegment.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+# Called when the bird collides with the pipe segment. 
+func _on_PipeSegment_body_entered(body: Node):
+	if body.has_method("die"):  # Duck-typing go brrr.
+		body.die()
