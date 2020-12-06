08d2a331a8fe56f7597e74f3c88d983b3bfbc751
Michelle S <progamer.firestorm@gmail.com>
2020-12-06 10:48:40 -0700

    Player movement

--- src/entities/bird/Bird.gd
+++ src/entities/bird/Bird.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+var velocity := Vector2.ZERO
+var strength_jump := 10.0
+var gravity := 9.8
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,5 +12,10 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _physics_process(_delta: float) -> void:
+	if (Input.is_action_just_pressed("ui_accept")):
+		velocity.y = -strength_jump
+	else:
+		velocity.y += gravity
+	
+	velocity = move_and_slide(velocity)
